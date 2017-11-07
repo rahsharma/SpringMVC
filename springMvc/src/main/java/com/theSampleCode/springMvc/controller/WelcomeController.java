8d221d6da9eea2609bb03e573fed5f6f47e390ca
rahsharma <imbusy.rahul@gmail.com>
2017-11-07 18:03:39 +0530

    Add files via upload

---
+++ springMvc/src/main/java/com/theSampleCode/springMvc/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.theSampleCode.springMvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping("/welcome")
+	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "welcome";
+	}
+
+}
