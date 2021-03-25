62c263354f0a2a6776b555d10ce4c8b415457e03
Sharma <rsharma2@ciena.com>
2021-03-25 13:31:36 +0530

    changes to testbranch

--- springMvc/src/main/java/com/theSampleCode/springMvc/SpringMvcApplication.java
+++ springMvc/src/main/java/com/theSampleCode/springMvc/SpringMvcApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringMvcApplication {
-
+//added comment to branch
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcApplication.class, args);
 	}
