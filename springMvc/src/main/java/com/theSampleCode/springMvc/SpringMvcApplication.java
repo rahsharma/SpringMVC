9069e9b3708ce79f8b1a0e30dca289817ea9d070
Sharma <rsharma2@ciena.com>
2021-03-25 13:38:37 +0530

    master comment

--- springMvc/src/main/java/com/theSampleCode/springMvc/SpringMvcApplication.java
+++ springMvc/src/main/java/com/theSampleCode/springMvc/SpringMvcApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringMvcApplication {
 //added comment to branch
+//added comment on master
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcApplication.class, args);
 	}
