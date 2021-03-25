8e93d6dec4788180b770b6106ffd4143a1bef464
Sharma <rsharma2@ciena.com>
2021-03-25 13:54:08 +0530

    correting merge issue

--- springMvc/src/main/java/com/theSampleCode/springMvc/SpringMvcApplication.java
+++ springMvc/src/main/java/com/theSampleCode/springMvc/SpringMvcApplication.java
@@ -6,11 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringMvcApplication {
 //added comment to branch
-<<<<<<< HEAD
 //added comment on master
-=======
 //added another comment to testbranch
->>>>>>> testbranch
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcApplication.class, args);
 	}
